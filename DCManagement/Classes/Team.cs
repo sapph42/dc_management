19b4e2cc728a8326020daf4a53514350a3c85e4f
nick.gibson <nicholas.j.gibson10.civ@health.mil>
2024-12-23 12:17:39 -0500

    More classes

---
+++ DCManagement/Classes/Team.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DCManagement.Classes; 
+internal class Team {
+    public int? TeamID { get; set; }
+    public Person? TeamLead { get; set; }
+    public Location? PrimaryLocation { get; set; }
+    public bool FillIfNoLead { get; set; } = true;
+    public bool Active { get; set; } = true;
+    public string TeamName { get; set; }
+    public Location? CurrentAssignment { get; set; }
+    public Team() {
+        TeamName = string.Empty;
+    }
+    public Team (string TeamName) {
+        this.TeamName = TeamName;
+    }
+}
